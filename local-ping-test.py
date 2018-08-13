59b34be642f63a83effd15ba55cccf6ff34e6e45
Rowell <rowelldionicio@users.noreply.github.com>
2018-08-13 00:37:38 -0700

    Create local-ping-test.py

---
+++ local-ping-test.py
@@ -0,0 +1,25 @@
+import subprocess
+from sys import argv
+
+script, filename = argv
+
+clients = open(filename)
+
+print("-" * 20)
+print("Performing ping test")
+print("-" * 20 + "\n")
+
+for host in clients:
+	ping = subprocess.run(["ping" + " -c 1 -t 2 " + str(host)], shell=True, 
+		stdout=subprocess.DEVNULL)
+	returncode = ping.returncode
+		
+	if returncode == 0:
+		print(host, "is UP \n")
+	
+	else:
+		print(f"{host} is DOWN \n")
+		
+print("Ping test complete.")
+
+clients.close()
